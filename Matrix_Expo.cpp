96064e405420c8a8b74973180f33662d167e0e90
raihan02 <raihanraju02@gmail.com>
2016-10-20 19:43:52 +0600

    Create Matrix_Expo.cpp

---
+++ Matrix_Expo.cpp
@@ -0,0 +1,224 @@
+ /*Header file starts here*/
+
+//#include <bits/stdc++.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cmath>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <climits>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <ctime>
+
+/*Header File ends here*/
+
+/*Macro starts here*/
+
+#define f(i,j,n) for(int i=j; i<n; i++)
+#define fu(i,j,n) for(int i=j; i>n; i--)
+#define c(x, n) x n; cin>>n
+#define ft int t; scanf("%d", &t); for(int c=1; c<=t; c++)
+#define w(n) while(n--)
+#define visited 1
+#define unvisited -1
+#define debug printf("\n<<CameHere<<\n")
+#define mem(x,y) memset(x, y, sizeof(x))
+#define pal(temp) pali(temp, 0, temp.size()-1)
+#define nl printf("\n")
+#define space printf(" ");
+#define eps 1e-9
+#define inf 1<<28
+#define cases printf("Case %d:", c);
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define min3(a,b,c) min(min(a,b),c)
+#define min4(a,b,c,d) min(min(a,b),min(c,d))
+#define max3(a,b,c) max(max(a,b),c)
+#define max4(a,b,c,d) max(max(a,b),max(c,d))
+#define fin(prob) freopen("prob.in", "r", stdin)
+#define fout(prob) freopen("prob.out", "w", stdout)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define cos(a) cos(a*pi/180)
+#define sin(a) sin(a*pi/180)
+#define tan(a) tan(a*pi/180)
+#define cosi(a) acos(a)/(pi/180)
+#define sini(a) asin(a)/(pi/180)
+#define tani(a) atan(a)/(pi/180)
+#define deb(x) cerr<< #x <<"="<<x<<endl;
+
+#define si(a) int a; scanf("%d", &a);
+#define sll(a) ll a; scanf("%lld", &a);
+#define sllu(a) unsigned ll a; scanf("%llu", &a);
+#define sd(a) double a; scanf("%lf", &a);
+
+#define pi(a) printf("%d", a);
+#define pll(a) printf("%lld", a);
+#define pllu(a) printf("%llu", a);
+#define pd(a) printf("%lf", a);
+
+//#define p printf
+/*Macro Ends here*/
+
+using namespace std;
+
+/*typedef start here*/
+
+const double pi=2*acos(0);
+
+typedef string st;
+typedef vector <int> vi;
+typedef vector <string> vs;
+typedef pair <int, int> ii;
+typedef long long ll;
+typedef vector < pair <int, int> > vii;
+typedef priority_queue<int,vector<int>,greater<int> > min_heap;
+
+/*typedef ends here*/
+
+/*Important functions starts here */
+
+template <class T> T abs (T a, T b=0)
+{
+    if(b==0) return (a<0) ? -a : a;
+    return (a>b) ? (a-b) : (b-a);
+}
+
+template <class T> inline T gcd(T a, T b)
+{
+    if(a<0) return gcd(-a, b);
+    if(b<0) return gcd(a,-b);
+    return (b==0) ? a : gcd(b, a%b);
+}
+
+ll xp(ll b, ll n, ll m=1<<63) {
+	int r = 1;
+	while(n > 0) {
+		if(n & 1) {
+			r = r * b;
+			if(r >= m) r %= m;
+		}
+		n >>= 1;
+		b = b * b;
+		if(b >= m) b %= m;
+	}
+	return r % m;
+}
+
+bool pali(string temp, int i, int j)
+{
+    if(i>=j) return true;
+    if(temp[i]==temp[j]) return pali(temp, i+1, j-1);
+    return false;
+}
+
+/*Important functions ends here*/
+
+/*Debug Extension ends here*/
+
+template <typename F, typename S>
+ostream& operator << (ostream& os, const pair< F, S>& p)
+{
+    return os<<"(" <<p.first<<", "<<p.second<<")";
+}
+
+template<typename T> ostream &operator << (ostream &os, const vector<T> &v)
+{
+    os<<"{";
+    typename vector<T> :: const_iterator it;
+    for(it= v.begin(); it!=v.end(); it++)
+    {
+        if(it!=v.begin()) os<<",";
+        os<<*it;
+    }
+    return os<<"}";
+}
+
+template <typename T> ostream &operator << (ostream & os, const set<T>&v)
+{
+    os<<"[";
+    typename set<T> :: const_iterator it;
+    for(it=v.begin(); it!=v.end(); it++)
+    {
+        if(it!=v.begin()) os<<",";
+        os<<*it;
+    }
+    return os<<"]";
+}
+
+template <typename F, typename S> ostream &operator << (ostream & os, const map<F,S>&v)
+{
+    os<<"[";
+    typename map<F,S>::const_iterator it;
+
+    for(it=v.begin(); it!=v.end(); it++)
+    {
+        if(it != v.begin()) os<<",";
+        os<< it ->first<<"="<<it->second;
+    }
+    return os<<"]";
+}
+
+struct Matrix{
+    int mat[4][4];
+    Matrix(){mem(mat, 0);}
+};
+
+void printmat(Matrix a){
+    f(i,0,2){
+        f(j,0,2) cout<<setw(4)<<a.mat[i][j];
+        nl;
+    }
+}
+
+Matrix matmul(Matrix a, Matrix b, int m){
+    Matrix c;
+    f(i,0,2) f(j,0,2) f(k,0,2) {
+        c.mat[i][j]+=(a.mat[i][k]%m * b.mat[k][j]%m)%m;
+        f(frst, 0, 2) f(scnd, 0, 2) c.mat[i][j]%m;
+    }
+    return c;
+}
+
+Matrix matpow(Matrix base, int p, int m){
+    Matrix ans;
+    f(i,0,2) f(j,0,2) ans.mat[i][j]= (i==j);
+    while(p){
+        if(p&1) ans=matmul(ans, base, m);
+        base=matmul(base, base, m);
+        p>>=1;
+    }
+    return ans;
+}
+
+int main(){
+    // fin(prob);
+    //fout(prob);
+    ft{
+        Matrix temp;
+        temp.mat[1][1]=temp.mat[0][1]=temp.mat[1][0]=1;
+        c(int, a);c(int, b);c(int, n);c(int,m);
+        int _m=1;
+        f(i,0,m) _m*=10;
+        m=_m;
+        temp=matpow(temp, n, m);
+      //  printmat(temp);
+        int ans=temp.mat[0][0]*a+temp.mat[0][1]*b;
+        ans%=m;
+        cases;space;cout<<ans<<endl;
+    }
+return 0;}
